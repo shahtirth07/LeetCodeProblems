d8e5c8862f576771d5b627900f028b6518742e88
Tirth Bhavesh Shah <shahtirth126@gmail.com>
2025-01-30 12:48:08 -0800

    Daily Temperatures

---
+++ Random/4.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> result(n, 0); // Initialize result array with 0s
+        stack<int> st; // Stack to store indices of temperatures
+
+        for (int i = 0; i < n; ++i) {
+            while (!st.empty() && temperatures[i] > temperatures[st.top()]) {
+                int prevIndex = st.top();
+                st.pop();
+                result[prevIndex] = i - prevIndex; // Compute the waiting days
+            }
+            st.push(i);
+        }
+
+        return result;
+    }
+};
